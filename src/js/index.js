d571fc0e5037dc79d68713e7457fd1cfb74911fb
Pablo Ballesteros Suarez <132361283+PabloBalle@users.noreply.github.com>
2023-07-13 11:23:29 +0000

    V1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,35 @@
-//import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const Trafficlight = () => {
 
+    const [light, setLight] = useState(null)
+
+    const myStyle = {
+        boxShadow: "0 0 80px #fff",        
+        animation:"glow 0.8s linear infinite alternate",        
+    }    
+    
+    const theRedClick = () => {
+        setLight("red")
+    }
+    const theYellowClick = () => {
+        setLight("yellow")
+    }
+    const theGreenClick = () => {
+        setLight("green")
+    }
+
+    return (
+        <div> 
+            <div className="box">          
+                <div className="light" id="red" onClick={theRedClick} style={light == "red" ? myStyle : null}></div>
+                <div className="light" id="yellow" onClick={theYellowClick} style={light == "yellow" ? myStyle : null}></div>
+                <div className="light" id="green" onClick={theGreenClick} style={light == "green" ? myStyle : null}></div>
+            </div> 
+        </div>
+    )
+}
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Trafficlight />, document.querySelector("#app"));
